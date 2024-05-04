343122a47702a59eb435e93f086f0268c4b5ef7e
hbaobaid00 <hishambaobaid@gmail.com>
2024-05-03 21:48:54 -0700

    made the audio play

--- assets/scripts/expose.js
+++ assets/scripts/expose.js
@@ -5,10 +5,19 @@ window.addEventListener('DOMContentLoaded', init);
 function init() {
   // TODO
   var selector = document.getElementById('horn-select');
-  
+  var audio = document.querySelector('audio');
+
   selector.addEventListener('change', function(){
-      var selectedHorn = selector.value;
+      var selectedHorn = selector.value; 
       var image = document.querySelector('img');
+
       image.src = 'assets/images/' + selectedHorn + '.svg';
+      audio.src = 'assets/audio/' + selectedHorn + '.mp3';
+  });
+
+  var button = document.querySelector('button');
+  
+  button.addEventListener('click', function(){
+      audio.play();
   });
 }
